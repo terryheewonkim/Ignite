bef7d6f783076e5994b70740abac54e51e8a4ff6
Terry Kim <terrykim025@gmail.com>
2021-05-23 16:40:01 +0900

    set up redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // REDUX SETUP
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+/*
+  With Redux, there is no way to do async JS (data fetching) in action creators
+  redux-thunk allows async fetching of data in action creators
+*/
 
-// Add store and redux devtools
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// Add store, redux devtools, thunk
+// applyMiddleware - checks every dispatch action and adds whatever is in the applyMiddleware (in this case, thunk)
+// compose - combine multiple arguments
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
