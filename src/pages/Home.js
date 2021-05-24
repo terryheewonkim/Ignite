a7512ef7f7d6329ad2ae47bdf43ec9a5f42c4a6a
Terry Kim <terrykim025@gmail.com>
2021-05-24 11:01:44 +0900

    Render games to page and add styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,81 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+// Redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+// Components
+import Game from "../components/Game";
+// Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { newGamesURL } from "../api";
 
 const Home = () => {
   // FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  // Get the data back
+  const { popular, newGames, upcomingGames } = useSelector(
+    (state) => state.games
+  );
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcomingGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>Popular Games</h2>
+      <Games>
+        {popular.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
